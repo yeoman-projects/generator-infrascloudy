b65fff479db83211db11d628da52ec559bc5a1e4
Allan Swanepoel <allanice001@gmail.com>
2019-08-01 07:18:43 +0100

    completing generator version2

--- generators/app/index.js
+++ generators/app/index.js
@@ -8,11 +8,11 @@ const kebabCase = require("lodash.kebabcase");
 const snakeCase = require("lodash.snakecase");
 
 module.exports = class extends Generator {
-  prompting() {
+  async prompting() {
     this.log(
       yosay(
         `O! I hear you would like a minimalistic scaffold with a React front-end, a Flask back-end, a gulpfile, Webpack config, Less, and Mocha tests
-        Welcome·to·the·neat·${chalk.green("infrascloudy")}·generator!`
+        Welcome to the ${chalk.green("infrascloudy")} generator!`
       )
     );
 
@@ -59,9 +59,98 @@ module.exports = class extends Generator {
   }
 
   writing() {
+    this.fs.copyTpl(
+      this.templatePath("gulpfile.js"),
+      this.destinationPath("gulpfile.js"),
+      {
+        date: new Date().toISOString().split("T")[0],
+        name: this.props.appName,
+        version: "1.0.0"
+      }
+    );
+
+    this.fs.copyTpl(
+      this.templatePath("_package.json"),
+      this.destinationPath("package.json"),
+      {
+        kebabName: this.kebabName,
+        snakeName: this.snakeName,
+        user: {
+          name: this.user.name,
+          email: this.user.email,
+          github: this.user.github
+        }
+      }
+    );
     this.fs.copy(
-      this.templatePath("dummyfile.txt"),
-      this.destinationPath("dummyfile.txt")
+      this.templatePath("gitignore"),
+      this.destinationPath(".gitignore")
+    );
+    this.fs.copyTpl(
+      this.templatePath("README"),
+      this.destinationPath("README.md"),
+      {
+        appName: this.kebabName
+      }
+    );
+    this.fs.copy(
+      this.templatePath("editorconfig"),
+      this.destinationPath(".editorconfig")
+    );
+    this.fs.copy(
+      this.templatePath("babelrc"),
+      this.destinationPath(".babelrc")
+    );
+    this.fs.copy(
+      this.templatePath("eslintrc"),
+      this.destinationPath(".eslintrc.json")
+    );
+    this.fs.copy(
+      this.templatePath("webpack.config.js"),
+      this.destinationPath("webpack.config.js")
+    );
+    this.fs.copyTpl(
+      this.templatePath("main.js"),
+      this.destinationPath("src/js/main.js"),
+      {
+        componentName: this.pascalName
+      }
+    );
+    this.fs.copy(
+      this.templatePath("component.jsx"),
+      this.destinationPath("src/js/components/" + this.kebabName + ".jsx")
+    );
+
+    this.fs.copyTpl(
+      this.templatePath("component-test.jsx"),
+      this.destinationPath("test/components/" + this.kebabName + "-test.jsx"),
+      {
+        componentName: this.pascalName,
+        kebabName: this.kebabName
+      }
+    );
+
+    this.fs.copy(
+      this.templatePath("api.py"),
+      this.destinationPath(this.snakeName + ".py")
+    );
+
+    this.fs.copyTpl(
+      this.templatePath("api_tests.py"),
+      this.destinationPath(this.snakeName + "_tests.py"),
+      {
+        appName: this.pascalName
+      }
+    );
+
+    this.fs.copy(
+      this.templatePath("index.html"),
+      this.destinationPath("templates/index.html")
+    );
+
+    this.fs.copy(
+      this.templatePath("requirements.txt"),
+      this.destinationPath("requirements.txt")
     );
   }
 
